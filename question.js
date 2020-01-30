0949b8a4ff77f1458faf3560b7d26845865287de
Jean Li <legacy812@gmail.com>
2020-01-30 15:16:55 -0800

    Merged files

--- question.js
+++ question.js
@@ -1,78 +1,91 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-const myQuestions = [
-  {
-    question: "What is the best color?",
-    answers: {
-      a: "Black",
-      b: "Midnight Blue",
-      c: "White, obviously"
-    },
-    correctAnswer: "b"
-  },
-  {
-    question: "What is the best site ever created?",
-    answers: {
-      a: "Facebook",
-      b: "Bootstrap",
-      c: "Trick question; quiz taker's preference is unknown"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "Where is Carmen Sandiego really?",
-    answers: {
-      a: "Antarctica",
-      b: "Exploring the Pacific Ocean",
-      c: "Around the world in 80 days",
-      d: "Minding her own business, so stop asking"
-    },
-    correctAnswer: "d"
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function () {
+  return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function (answer) {
+  if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+    this.score++;
   }
+
+  this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function () {
+  return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+  return this.answer === choice;
+}
+
+
+function populate() {
+  if (quiz.isEnded()) {
+    showScores();
+  }
+  else {
+    // show question
+    var element = document.getElementById("question");
+    element.innerHTML = quiz.getQuestionIndex().text;
+
+    // show options
+    var choices = quiz.getQuestionIndex().choices;
+    for (var i = 0; i < choices.length; i++) {
+      var element = document.getElementById("choice" + i);
+      element.innerHTML = choices[i];
+      guess("btn" + i, choices[i]);
+    }
+
+    showProgress();
+  }
+};
+
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function () {
+    quiz.guess(guess);
+    populate();
+  }
+};
+
+
+function showProgress() {
+  var currentQuestionNumber = quiz.questionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+  var gameOverHTML = "<h1>Result</h1>";
+  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+  var element = document.getElementById("quiz");
+  element.innerHTML = gameOverHTML;
+};
+
+// create questions here
+var questions = [
+  new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML", "CSS", "HTML"], "HTML"),
+  new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+  new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery", "Django", "NodeJS"], "Django"),
+  new Question("What is does an array mean?", ["Images", "Pieces of Data", "Element", "All"], "Pieces of Data"),
+  new Question("What does # stand for in JS?", ["Web Design", "ID tag", "Class", "Twitter"], "ID Tag")
 ];
 
-function buildQuiz() {
-  // we'll need a place to store the HTML output
-  const output = [];
+// create quiz
+var quiz = new Quiz(questions);
 
-  // for each question...
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-
-
-      // we'll want to store the list of answer choices
-      const answers = [];
-
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-      );
-    }
-  );
-
-  // finally combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');
- }
-
-function showResults() { }
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
+// display quiz
+populate();
